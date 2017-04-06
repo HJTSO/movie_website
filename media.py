661b0328b1d1fa8cd8c340cabcece281929be074
HJT <562784738@qq.com>
2017-04-07 00:54:06 +0900

    Add files via upload

--- media.py
+++ media.py
@@ -1,14 +1,19 @@
 class Movie():
-    def __init__(self,movie_title,movie_storyline,poster_image,
-                 trailer_youtube):
-        self.title = movie_title
-        self.storyline = movie_storyline
-        self.poster_image_url = poster_image
-        self.trailer_youtube_url = trailer_youtube
+ """Description of Movie: Build a movie which included title,storyline and other information"""
 
-'''
-    def show_trailer(self):
-        print self.trailer_youtube_url
-    def show_info(self):
-        print movie_storyline
-'''
+ def __init__(self,movie_title,movie_storyline,poster_image,
+                 trailer_youtube):
+  """Method description
+        Args:
+            self => string, argument self
+            movie_title => string, title
+            movie_storyline => string, storyline
+            poster_image => string, poster_image_url
+            trailer_youtube => string, trailer_youtube_url
+        Return:
+             None
+  """
+  self.title = movie_title
+  self.storyline = movie_storyline
+  self.poster_image_url = poster_image
+  self.trailer_youtube_url = trailer_youtube
